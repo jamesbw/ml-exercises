9d5b5d609dc56bf4d462d6a8f1558f430567b1c8
jamesbw <github+jamesbw@squareup.com>
2014-07-29 14:40:54 -0700

    Decision tree exercise

---
+++ decision-tree/IMPLEMENT_ME.js
@@ -0,0 +1,50 @@
+/* Class definitions */
+
+/* A datapoint has features and a label.
+  featureMap is a hash of features to values
+  label is either Label.FRAUD or Label.NOT_FRAUD
+*/
+function Datapoint(featureMap, label) {
+  this.featureMap = featureMap;
+  this.label = label;
+}
+
+/* Label "enum". The freeze prevents these values from being changed.
+  You should be able to refer simply to Label.FRAUD and Label.NOT_FRAUD.
+ */
+var Label = Object.freeze({
+  FRAUD: "FRAUD",
+  NOT_FRAUD: "NOT_FRAUD"
+});
+
+/* Features "enum". The freeze prevents these values from being changed. */
+var Features = Object.freeze({
+  NUM_CHARGEBACKS: "NUM_CHARGEBACKS",
+  AGE_OF_ACCOUNT_IN_DAYS: "AGE_OF_ACCOUNT_IN_DAYS",
+  DECLINE_RATE: "DECLINE_RATE",
+  CARD_DIVERSITY: "CARD_DIVERSITY"
+});
+
+/* IMPLEMENT THE FUNCTIONS BELOW
+--------------------------------
+*/
+
+/*
+  The Gini impurity of a set is defined as 1 - sum(freq_i^2), where freq_i are the frequencies for each class.
+  In our case, we have two classes, the fraud and non-fraud classes.
+  The frequency of a given class is the num of times it occurs in the dataset, divided by the total size of the dataset.
+  For example, if there are 4 fraud datapoints and 6 non-fraud datapoints, the frequency of fraud is 0.4 and the frequency of non-fraud is 0.6.
+  The Gini impurity in this case would be 1 - 0.4 * 0.4 - 0.6 * 0.6 = 0.48
+*/
+// function calculateGiniImpurityOfSet(datapoints) {
+//   return 0.3;
+// }
+
+/*
+  The Gini impurity of a split is the average of the Gini impurities of each subset,
+  weighted by the size of each subset.
+*/
+// function calculateGiniImpurityOfSplit(datapoints1, datapoints2) {
+//   return 0.12;
+// }
+
